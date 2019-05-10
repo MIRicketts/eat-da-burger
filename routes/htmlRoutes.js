b807fc5016bf43309c5ddae384b592a4433a181c
Marsden Ricketts <marsdenrickett@gmail.com>
2019-05-09 21:20:26 -0400

    local working

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
 
     burgers.findAll()
     .then(dbBurgerData => {
-      res.render("index", { burgerData, dbBurgerData})
+      res.render("index", { burgers, dbBurgerData})
     })
     .catch(err => {
       console.log(err);
